6580d9106161b4a9e5e60eab93903b041745a6f4
Roland4444 <turbopollux@rambler.ru>
2019-03-28 13:54:50 +0400

    CBlasted rewrites to C++

--- def.hpp
+++ def.hpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <dlfcn.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #ifndef DEF_HPP_
 #define DEF_HPP_
-
-using namespace std;
+#define soundso "./libsqc.so"
+#define photoso "./libBKKAdaptor.so"
+#define soundconfig "./cv_configuration.json"
+#define photoconfig "./ci_configuration.json"
+#define soundindex 0
+#define photoindex 1
 
 typedef struct{
 	int major;
@@ -18,9 +26,13 @@ typedef struct{
 	int last_error;
 } Session;
 
-typedef int (*v_create_session)(Session*, char *);
-typedef int (*BKKCheck)(Session *,  uint8_t * , uint64_t);
+typedef int (*BKKCheck)(Session*,  uint8_t* , uint64_t);
+typedef int (*create_session)(struct Session*  , char*);
+typedef int (*BKKCheck)(Session*,  uint8_t* , uint64_t);
 
+typedef int (*BKK)(Session*,  char*);
+typedef int (*i_check_format)(Session*,  uint8_t*, uint64_t);
+typedef char* (*v_session_configuration_version)(struct Session*);
 
 typedef struct{
 	uint8_t* content;
--- main.cpp
+++ main.cpp
@@ -14,105 +14,166 @@ public:
 	}
 };
 
-class Checker_Audio
+
+class ContentInfo{
+	uint8_t* content;
+	uint64_t size;
+};
+
+class Checker
 {
-	public:
-		Session * session;
-		int ResultCreating;
-		void * handle;
-        char * config = "./cv_configuration.json";
+public:
+	void* handles[2];
+	Session* sessions[2];
+	BKKCheck v_check;
+	BKKCheck i_check;
 
-        int checkFile(ContentInfo * ci){
-          BKKCheck v_check = (BKKCheck)(dlsym(handle,"v_check"));
-          if (!v_check)
-        	  return -2;
+	int checkFile_(Checker* self, char* filename)
+	{
+	  ContentInfo* ci = self->loadContent(filename);
+	  if (ci == NULL)
+	      return -1;
+	  printf("SIZE CONTENT==%d\n\n\n", ci->size);
+	  if (strstr(filename, "wav")!=NULL){
+	    printf("CHECKING WAV FILE %s", filename);
+	    if (!self->v_check(self->sessions[soundindex], ci->content, ci->size))
+	    {
+		delete(ci);
+		printf("Check failed!\n");
+		return -3;
+	    }
+	    else
+	    {
+	      printf("Checking passed\n");
+	      delete(ci);
+	      return 0;
+	    }
+	  }
+	  printf("CHECKING photo FILE %s", filename);
+	  if (!self->i_check(self->sessions[photoindex], ci->content, ci->size))
+	  {
+	      printf("Check failed!\n");
+	      delete(ci);
+	      return -3;
+	  }
+	  else
+	  {
+	      printf("Checking passed\n");
+	      delete(ci);
+	      return 0;
+	  }
+	  delete(ci);
+	  return -9;
 
-          if (!v_check(this->session, ci ->content, ci ->sizecontent)){
-        	  std::cout<<"Check failed!";
-              return -3;
-          }
-          else return 0;
-        }
+	}
 
-        int checkFile(char * filename){
-		    std::cout<<"\n\nchecking =>"<<filename<<endl;
-		    ContentInfo * ci;
-			if (loadContent(filename) == NULL)
-                return -1;
-            ci = loadContent(filename);
-            BKKCheck v_check = (BKKCheck)(dlsym(handle,"v_check"));
-            if (!v_check)
-                return -2;
+	Checker()
+	{
+	  this->handles[soundindex] = dlopen(soundso, RTLD_LAZY);
+	  this->handles[photoindex] = dlopen(photoso, RTLD_LAZY);
+	  if (!this->handles[soundindex])
+		    printf("error loading sound so");
+	  else
+		    printf("Load success sound so");
 
-            if (!v_check(this->session, ci ->content, ci ->sizecontent)){
-                std::cout<<"Check failed!";
-                return -3;
-            }
-            else return 0;
-		}
+	  if (!this->handles[photoindex])
+		    printf("error loading photo so");
+		  else
+		    printf("Load success photo so");
 
-		Checker_Audio()
-		{
-			this -> session = (Session*)malloc(sizeof(Session));
-			std::cout<<"Loads "<<sizeof(Session)<<"  mamory\n";
-			this -> handle = dlopen("./libsqc.so", RTLD_LAZY);
-			ResultCreating=0;
-			if (handle == NULL)
-			{
-				std::cout<<"\n\nerror loading\n\n\n";
-				ResultCreating = -1;
-			}
-			initSession();
-		};
+	  this->initSessions();
+	  this->loadcheckers();
+	  printf("\n\n\nVERSION===>\n\n%s\n\n\n\n",  getVersion());
+
+
+	};
     private:
-        ContentInfo * loadContent(char * filename)
-        {
-            ContentInfo * ci=new ContentInfo;
-            read_file_content(filename, &ci ->content, &ci->sizecontent);
-            return ci;
-        }
-        void initSession()
-        {
-            Session * session = (Session*)malloc(sizeof(Session));
-            v_create_session load = (v_create_session)(dlsym(this->handle, "v_create_session"));
-            if (!load){
-                std::cout<<"error loading v_create_session";
-                this->session=NULL;
-            }
-            session->id="my_session";
-            if (!load(session, config)){
-                std::cout<<"error create session";
-                this->session=NULL;
-                return;
-            }
-            std::cout<<"Session creates succesfully";
-            this->session=session;
-        }
 
-        bool read_file_content(const char *file_path, uint8_t **content, size_t *content_size) {
-            FILE *fd = fopen(file_path, "rb");
-            if (fd == NULL) {
-                fprintf(stderr, "file \"%s\" not found\n", file_path);
-                return false;
-            }
-            fseek(fd, 0L, SEEK_END);
-            (*content_size) = (size_t) ftell(fd);
-            rewind(fd);
-            (*content) = (uint8_t *) calloc(1, (*content_size));
-            fread((*content), (*content_size), 1, fd);
-            fclose(fd);
-            return true;
-        }
+	int read_file_content(const char* file_path, uint8_t** content, size_t* content_size)
+	{
+	  FILE* fd = fopen(file_path, "rb");
+	  if (fd == NULL)
+	  {
+	    fprintf(stderr, "file \"%s\" not found\n", file_path);
+	    return 0;
+	  }
+	  fseek(fd, 0L, SEEK_END);
+	  (*content_size) = (size_t) ftell(fd);
+	  rewind(fd);
+	  (*content) = (uint8_t*) calloc(1, (*content_size));
+	  fread((*content), (*content_size), 1, fd);
+	  fclose(fd);
+	  return 1;
+	}
+
+	ContentInfo* loadContent(char * filename)
+	{
+	  ContentInfo* ci=new ContentInfo();
+	  if (read_file_content(filename, &ci ->content, &ci->size))
+	    return ci;
+	  delete(ci);
+	  return NULL;
+	}
+
+	char* getVersion()
+	{
+	  v_session_configuration_version get_sess_version = (v_session_configuration_version)(dlsym(self->handles[soundindex],"v_session_configuration_version"));
+	  return get_sess_version(sessions[soundindex]);
+
+	}
+
+	void loadcheckers(){
+	  this->i_check = (BKKCheck)(dlsym(handles[photoindex],"i_check_format"));
+	  this->v_check = (BKKCheck)(dlsym(handles[soundindex],"v_check"));
+	  if (!i_check)
+	    printf("error load i_check");
+	  if (!v_check)
+	    printf("error load v_check");
+
+	}
+
+	Session* initSession(void* handle, char* symbol, char* config)
+	{
+	  Session* sess = (Session*)malloc(sizeof(Session));
+	  create_session load = (create_session)(dlsym(handle, symbol));
+	  if (!load){
+	    printf("error loading %s\n\n\n", symbol );
+	    sess=NULL;
+	  }
+	  sess->id="my_session";
+	  if (!load(sess, config)){
+	    printf("error create session\n\n");
+	    sess=NULL;
+	    return sess;
+	  }
+	  printf("Session creates succesfully\n\n");
+	  printf("\n\nVERSION=>>\nMajor %d, minor %d, build %d\n\n\n", sess->version.major, sess->version.minor, sess->version.build);
+	  return sess;
+	}
+
+	void initSessions()
+	{
+	  this->sessions[photoindex]=initSession(handles[photoindex], "i_create_session", photoconfig);
+	  this->sessions[soundindex]=initSession(handles[soundindex], "v_create_session", soundconfig);
+	  int i;
+	  for (i=0;i<2; i++){
+	    if (sessions[i]==NULL)
+	      printf("Error create session #%d\n", i);
+	  }
+
+
+	}
+
 
 };
 summer* sum;
-Checker_Audio* ca;
+Checker* ca;
 extern "C"
 {
 void init()
 {
 	sum = new summer();
-	ca = new Checker_Audio();
+	ca = new Checker();
 }
 
 void call()
